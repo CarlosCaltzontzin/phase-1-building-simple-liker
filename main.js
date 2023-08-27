7b909ab5750d114a05e1511b0e4cd160e4a421bd
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-08-26 23:18:49 -0500

    completed phase-1-building-simple-liker

--- main.js
+++ main.js
@@ -3,9 +3,56 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  // Step 1: Add an event listener to the heart icons
+  const heartIcons = document.querySelectorAll(".like-glyph");
+  heartIcons.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      handleLike(heart);
+    });
+  });
 
+  // Step 2: Handle the like action
+  function handleLike(heart) {
+    mimicServerCall()
+      .then(() => {
+        // Server request success
+        toggleHeart(heart);
+      })
+      .catch((error) => {
+        // Server request failure
+        displayErrorModal(error);
+      });
+  }
 
+  // Step 3: Update the heart icon
+  function toggleHeart(heart) {
+    if (heart.textContent === EMPTY_HEART) {
+      heart.textContent = FULL_HEART;
+      heart.classList.add("activated-heart");
+    } else {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    }
+  }
 
+  // Step 4: Display error modal
+  function displayErrorModal(errorMessage) {
+    const errorModal = document.querySelector("#modal");
+    const modalMessage = document.querySelector("#modal-message");
+    
+    // Set the error message
+    modalMessage.textContent = errorMessage;
+
+    // Remove the .hidden class to show the modal
+    errorModal.classList.remove("hidden");
+    
+    // Hide the modal after 3 seconds
+    setTimeout(() => {
+      errorModal.classList.add("hidden");
+    }, 3000);
+  }
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
